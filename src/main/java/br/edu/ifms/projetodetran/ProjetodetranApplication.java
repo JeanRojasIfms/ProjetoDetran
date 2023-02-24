70c14aa743b660e2e4cf741129863424b75ba759
unknown <jean.rojas@ifms.edu.br>
2023-02-23 20:02:59 -0400

    criação da camada de modelo

---
+++ src/main/java/br/edu/ifms/projetodetran/ProjetodetranApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.projetodetran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodetranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodetranApplication.class, args);
+	}
+
+}
